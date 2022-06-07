0c3059d12fbf3ba9274c22bde047df33b1573742
zxg <975203723@qq.com>
2022-06-07 11:03:08 +0800

    feat(common): :tada: IO

---
+++ IO.py
@@ -0,0 +1,8 @@
+print('A', 'B', 'C', 'D', 'E', 'F', 'G')
+# 输出 A B C D E F G
+# 遇到逗号，输出一个空格
+
+# 输入
+# input()和print()是在命令行下面最基本的输入和输出
+name = input('Please input your name: ')
+print('Hello,', name)
